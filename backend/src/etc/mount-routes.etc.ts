bfb306583113ea9995d2293bcef332fab559c231
ShovalShabi <shovalshabi@gmail.com>
2025-01-15 21:12:55 +0200

    mounted instructors route

--- backend/src/etc/mount-routes.etc.ts
+++ backend/src/etc/mount-routes.etc.ts
@@ -1,7 +1,9 @@
 import { Express } from "express";
 import lessonRouter from "../route/lesson.route.js";
+import instructorRouter from "../route/instructor.route.js";
 
 export function mountRoutes(app: Express) {
   // Mount routes under their respective base paths
   app.use("/lesson", lessonRouter);
+  app.use("/instructor", instructorRouter);
 }
