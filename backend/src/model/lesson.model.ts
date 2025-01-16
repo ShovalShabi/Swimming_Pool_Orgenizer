db2e49c6d91c234b3510978335c01076e2f8c1af
ShovalShabi <shovalshabi@gmail.com>
2025-01-16 17:24:18 +0200

    Changed lesson model according to the DTO changes

--- backend/src/model/lesson.model.ts
+++ backend/src/model/lesson.model.ts
@@ -2,6 +2,7 @@ import mongoose, { Schema, Document, Model } from "mongoose";
 import { LessonType } from "../utils/lesson-enum.utils.js";
 import { Swimming } from "../utils/swimming-enum.utils.js";
 import Student from "../dto/student/student.dto.js";
+import StartAndEndTime from "../dto/instructor/start-and-end-time.dto.js";
 
 // Define the Student Schema
 const StudentSchema = new Schema<Student>({
@@ -18,25 +19,25 @@ const StudentSchema = new Schema<Student>({
   },
 });
 
-// Define an interface for Lesson documents
+// Define the StartAndEndTime Schema
+const StartAndEndTimeSchema = new Schema<StartAndEndTime>({
+  startTime: { type: Date, required: true },
+  endTime: { type: Date, required: true },
+});
+
+// Interface for Mongoose Schema
 export interface ILesson extends Document {
-  lessonId: string;
+  _id: mongoose.Types.ObjectId;
   typeLesson: LessonType;
   specialties: Swimming[];
-  instructorId: string;
-  dateAndTime: Date;
-  duration: number;
+  instructorId: mongoose.Types.ObjectId; // ObjectId reference to Instructor
+  startAndEndTime: StartAndEndTime;
   students: Student[];
 }
 
 // Define the Lesson Schema
-const LessonSchema = new Schema(
+const LessonSchema = new Schema<ILesson>(
   {
-    lessonId: {
-      type: String,
-      required: true,
-      unique: true,
-    },
     typeLesson: {
       type: String,
       enum: Object.values(LessonType),
@@ -48,19 +49,24 @@ const LessonSchema = new Schema(
       required: true,
     },
     instructorId: {
-      type: String,
+      type: Schema.Types.ObjectId, // Reference to the Instructor model
+      ref: "Instructor",
       required: true,
     },
-    dateAndTime: {
-      type: Date,
-      required: true,
-    },
-    duration: {
-      type: Number,
+    startAndEndTime: {
+      type: StartAndEndTimeSchema,
       required: true,
     },
     students: {
       type: [StudentSchema],
+      validate: {
+        validator: (students: Student[]) =>
+          Array.isArray(students) &&
+          students.length > 0 &&
+          students.length <= 30,
+        message:
+          "Students must be a non-empty array and must not exceed 30 in size.",
+      },
       required: true,
     },
   },
