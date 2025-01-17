519884fe6b5adaa6744977a3c919155e7d9a0a92
ShovalShabi <shovalshabi@gmail.com>
2025-01-17 21:08:21 +0200

    Corrected lessons service logic

--- backend/src/service/lesson/lesson.service.ts
+++ backend/src/service/lesson/lesson.service.ts
@@ -11,6 +11,7 @@ import Instructor from "../../dto/instructor/instructor.dto.js";
 import StartAndEndTime from "../../dto/instructor/start-and-end-time.dto.js";
 import { DaysOfWeek } from "../../utils/days-week-enum.utils.js";
 import LessonServiceInterface from "./ILesson.service.js";
+import compareTime from "../../utils/compare-hours.utils.js";
 
 export default class LessonService implements LessonServiceInterface {
   private lessonRepository: LessonRepositoryInterface;
@@ -37,7 +38,7 @@ export default class LessonService implements LessonServiceInterface {
       await this.instructorService.getInstructorById(lessonData.instructorId); // must be valid otherwise it will throw an exception
 
     if (
-      lessonData.specialties.every((specialty) =>
+      !lessonData.specialties.every((specialty) =>
         instructorData.specialties.includes(specialty)
       )
     ) {
@@ -53,16 +54,14 @@ export default class LessonService implements LessonServiceInterface {
         instructorData.availabilities[dayOfTheWeek].startTime;
       const instEndTime = instructorData.availabilities[dayOfTheWeek].endTime;
       if (
-        !(
-          lessonData.startAndEndTime.startTime >= instStartTime &&
-          lessonData.startAndEndTime.endTime <= instEndTime
-        )
+        compareTime(lessonData.startAndEndTime.startTime, instStartTime) < 0 || // Start time is earlier than available
+        compareTime(lessonData.startAndEndTime.endTime, instEndTime) > 0 // End time is later than available
       )
         // if the instructor is not teaching in those time slots
         throw new createHttpError.BadRequest(
           `The Instructor ${
             instructorData.name
-          } is available only for ${instStartTime.getHours()}:${instStartTime.getMinutes()} - ${instEndTime.getHours()}:${instEndTime.getMinutes()} on ${
+          } is available only for ${instStartTime.getUTCHours()}:${instStartTime.getUTCMinutes()} - ${instEndTime.getUTCHours()}:${instEndTime.getUTCMinutes()} on ${
             Object.values(DaysOfWeek)[dayOfTheWeek]
           }`
         );
@@ -71,12 +70,24 @@ export default class LessonService implements LessonServiceInterface {
         instructorData.instructorId!,
         lessonData.startAndEndTime.endTime
       );
-      this.validateOverlapingLessons(lessonData, exisitngLessons);
+      this.validateOverlappingLessons(lessonData, exisitngLessons);
+      const lesson: Lesson = new Lesson(
+        null, // lessonId will be assigned by the database
+        lessonData.typeLesson,
+        lessonData.specialties,
+        lessonData.instructorId,
+        lessonData.startAndEndTime,
+        lessonData.students
+      );
 
-      return this.lessonRepository.createLesson(lessonData);
+      return this.lessonRepository.createLesson(lesson);
     }
 
-    return this.lessonRepository.createLesson(lessonData);
+    throw new createHttpError.BadRequest(
+      `The Instrucor ${instructorData.name} is not teaching on ${
+        Object.values(DaysOfWeek)[dayOfTheWeek]
+      }`
+    );
   }
 
   async getLessonById(lessonId: string): Promise<Lesson> {
@@ -84,7 +95,7 @@ export default class LessonService implements LessonServiceInterface {
 
     if (!lesson)
       throw new createHttpError.NotFound(
-        `Instructor with ID ${lessonId} not found`
+        `Lesson with ID ${lessonId} not found`
       );
     return lesson;
   }
@@ -119,7 +130,10 @@ export default class LessonService implements LessonServiceInterface {
     return filteredLessons;
   }
 
-  async updateLesson(lessonId: string, lessonData: Lesson): Promise<boolean> {
+  async updateLesson(
+    lessonId: string,
+    lessonData: Lesson
+  ): Promise<Lesson | null> {
     const lesson = await this.getLessonById(lessonId);
 
     if (!lesson) {
@@ -134,7 +148,7 @@ export default class LessonService implements LessonServiceInterface {
       await this.instructorService.getInstructorById(lessonData.instructorId); // must be valid otherwise it will throw an exception
 
     if (
-      lessonData.specialties.every((specialty) =>
+      !lessonData.specialties.every((specialty) =>
         instructorData.specialties.includes(specialty)
       )
     ) {
@@ -152,16 +166,14 @@ export default class LessonService implements LessonServiceInterface {
         instructorData.availabilities[dayOfTheWeek].startTime;
       const instEndTime = instructorData.availabilities[dayOfTheWeek].endTime;
       if (
-        !(
-          lessonData.startAndEndTime.startTime >= instStartTime &&
-          lessonData.startAndEndTime.endTime <= instEndTime
-        )
+        compareTime(lessonData.startAndEndTime.startTime, instStartTime) < 0 || // Start time is earlier than available
+        compareTime(lessonData.startAndEndTime.endTime, instEndTime) > 0 // End time is later than available
       )
         // if the instructor is not teaching in those time slots
         throw new createHttpError.BadRequest(
           `The Instructor ${
             instructorData.name
-          } is available only for ${instStartTime.getHours()}:${instStartTime.getMinutes()} - ${instEndTime.getHours()}:${instEndTime.getMinutes()} on ${
+          } is available only for ${instStartTime.getUTCHours()}:${instStartTime.getUTCMinutes()} - ${instEndTime.getUTCHours()}:${instEndTime.getUTCMinutes()} on ${
             Object.values(DaysOfWeek)[dayOfTheWeek]
           }`
         );
@@ -170,19 +182,25 @@ export default class LessonService implements LessonServiceInterface {
         instructorData.instructorId!,
         lessonData.startAndEndTime.endTime
       );
-      this.validateOverlapingLessons(lessonData, exisitngLessons);
+      this.validateOverlappingLessons(lessonData, exisitngLessons);
+
+      const updatedLesson: Lesson = {
+        instructorId: lessonData.instructorId,
+        lessonId,
+        specialties: [...lessonData.specialties],
+        typeLesson: lessonData.typeLesson,
+        startAndEndTime: lessonData.startAndEndTime,
+        students: [...lessonData.students],
+      };
+
+      return this.lessonRepository.updateLesson(lessonId, updatedLesson);
     }
 
-    const updatedLesson: Lesson = {
-      instructorId: lessonData.instructorId,
-      lessonId,
-      specialties: [...lessonData.specialties],
-      typeLesson: lessonData.typeLesson,
-      startAndEndTime: lessonData.startAndEndTime,
-      students: [...lessonData.students],
-    };
-
-    return this.lessonRepository.updateLesson(lessonId, updatedLesson);
+    throw new createHttpError.BadRequest(
+      `The Instrucor ${instructorData.name} is not teaching on ${
+        Object.values(DaysOfWeek)[dayOfTheWeek]
+      }`
+    );
   }
 
   async deleteLesson(lessonId: string): Promise<boolean> {
@@ -198,7 +216,7 @@ export default class LessonService implements LessonServiceInterface {
       throw new createHttpError.BadRequest(
         `Invalid specialty: ${
           lessonData.typeLesson
-        }. Must be one of: ${Object.values(Swimming).join(", ")}`
+        }. Must be one of: ${Object.values(LessonType).join(", ")}`
       );
     }
 
@@ -227,67 +245,81 @@ export default class LessonService implements LessonServiceInterface {
     )
       throw new createHttpError.BadRequest("Invalid date parameter.");
 
+    // Validate lesson duration based on type
+    const startTime = lessonData.startAndEndTime.startTime.getTime();
+    const endTime = lessonData.startAndEndTime.endTime.getTime();
+    const durationInMinutes = (endTime - startTime) / (1000 * 60);
+
+    if (
+      (lessonData.typeLesson === LessonType.MIXED ||
+        lessonData.typeLesson === LessonType.PRIVATE) &&
+      durationInMinutes !== 60
+    ) {
+      throw new createHttpError.BadRequest(
+        `Invalid duration for ${lessonData.typeLesson} lesson. It must last exactly 1 hour.`
+      );
+    }
+
+    if (
+      lessonData.typeLesson === LessonType.PUBLIC &&
+      durationInMinutes !== 45
+    ) {
+      throw new createHttpError.BadRequest(
+        `Invalid duration for PUBLIC lesson. It must last exactly 45 minutes.`
+      );
+    }
+
     if (lessonData.students.length === 0 || lessonData.students.length > 30) {
       throw new createHttpError.BadRequest(
         "The number of students taking the lesson must be between 1 to 30."
       );
     }
+
+    lessonData.students.map((student) => {
+      if (lessonData.typeLesson !== student.lessonType) {
+        throw new createHttpError.BadRequest(
+          `The student must take the same type of lesson, student chose ${student.lessonType} while the lesson is ${lessonData.typeLesson}`
+        );
+      }
+
+      if (
+        !student.preferences.every((preference) =>
+          lessonData.specialties.includes(preference)
+        )
+      ) {
+        throw new createHttpError.BadRequest(
+          `The lesson is not answering the full requirements of the student which wanted ${Object.values(
+            student.preferences
+          ).join(", ")} while the lesson offers ${Object.values(
+            lessonData.specialties
+          ).join(", ")}`
+        );
+      }
+    });
   };
 
-  validateOverlapingLessons = (
+  validateOverlappingLessons = (
     targetLesson: Lesson | NewLesson,
     arrExistingLessons: Lesson[]
   ) => {
-    const targetStart = targetLesson.startAndEndTime.startTime.getTime();
-    const targetEnd = targetLesson.startAndEndTime.endTime.getTime();
-
-    const violations: boolean[] = []; // Collect all violation messages
+    const targetStart = targetLesson.startAndEndTime.startTime;
+    const targetEnd = targetLesson.startAndEndTime.endTime;
 
     arrExistingLessons.forEach((existingLesson) => {
-      const existingStart = existingLesson.startAndEndTime.startTime.getTime();
-      const existingEnd = existingLesson.startAndEndTime.endTime.getTime();
+      const existingStart = existingLesson.startAndEndTime.startTime;
+      const existingEnd = existingLesson.startAndEndTime.endTime;
 
-      // Check if time ranges overlap
-      const isOverlapping =
-        targetStart < existingEnd && targetEnd > existingStart;
+      // Check if the time ranges overlap using compareTime
+      const startComparison = compareTime(targetStart, existingEnd);
+      const endComparison = compareTime(targetEnd, existingStart);
+
+      const isOverlapping = startComparison < 0 && endComparison > 0;
 
       if (isOverlapping) {
-        // Rule 1: Private lesson cannot overlap another private lesson
-        if (
-          targetLesson.typeLesson === LessonType.PRIVATE &&
-          existingLesson.typeLesson === LessonType.PRIVATE
-        ) {
-          violations.push(true);
-        }
-
-        // Rule 2: Public and Mixed can overlap each other
-        if (
-          (targetLesson.typeLesson === LessonType.PUBLIC &&
-            existingLesson.typeLesson === LessonType.MIXED) ||
-          (targetLesson.typeLesson === LessonType.MIXED &&
-            existingLesson.typeLesson === LessonType.PUBLIC)
-        ) {
-          // Overlap allowed, skip to the next check
-          return;
-        }
-
-        // Rule 3: Private lessons cannot overlap public or mixed lessons unless the existing one is mixed
-        if (
-          (targetLesson.typeLesson === LessonType.PRIVATE &&
-            existingLesson.typeLesson !== LessonType.MIXED) ||
-          (existingLesson.typeLesson === LessonType.PRIVATE &&
-            targetLesson.typeLesson !== LessonType.MIXED)
-        ) {
-          violations.push(true);
-        }
+        throw new createHttpError.BadRequest(
+          `Overlapping lessons detected between new lesson and existing lesson ${existingLesson.lessonId}`
+        );
       }
     });
-
-    // Throw an exception if there are any violations
-    if (violations.length > 0) {
-      throw new Error(
-        `Overlapping lessons detected: \n${violations.join("\n")}`
-      );
-    }
   };
 }
