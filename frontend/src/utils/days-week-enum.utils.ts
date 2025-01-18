bad58e77f045ad9b6bcedf270e293328ffb13241
ShovalShabi <shovalshabi@gmail.com>
2025-01-18 12:14:32 +0200

    Added utility types and enums

---
+++ frontend/src/utils/days-week-enum.utils.ts
@@ -0,0 +1,9 @@
+export enum DaysOfWeek {
+  SUN = "Sunday",
+  MON = "Monday",
+  TUE = "Tuesday",
+  WED = "Wednesday",
+  THU = "Thursday",
+  FRI = "Friday",
+  SAT = "Saturday",
+}
---
+++ frontend/src/utils/swimming-enum.utils.ts
@@ -0,0 +1,6 @@
+export enum Swimming {
+  CHEST = "CHEST",
+  BACK_STROKE = "BACK_STROKE",
+  BUTTERFLY_STROKE = "BUTTERFLY_STROKE",
+  ROWING = "ROWING",
+}
