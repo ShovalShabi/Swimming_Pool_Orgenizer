310a9f2bc75f8011780e41b451d53777d2d3c80a
ShovalShabi <shovalshabi@gmail.com>
2025-01-18 14:43:12 +0200

    Allowed access from any origin

--- backend/src/etc/cors.etc.ts
+++ backend/src/etc/cors.etc.ts
@@ -2,10 +2,14 @@ import { CorsOptions } from "cors"; // Import the type for CorsOptions
 
 // Define CORS options with proper typing
 const corsOptions: CorsOptions = {
-  origin: [
-    process.env.CLIENT_DEV_URL as string,
-    process.env.CLIENT_PROD_URL as string,
-  ],
+  // origin: [
+  //   "*",
+  //   process.env.CLIENT_DEV_URL as string,
+  //   process.env.CLIENT_PROD_URL as string,
+  // ],
+  origin: (origin, callback) => {
+    callback(null, true); // Allow all origins
+  },
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // Allow cookies to be sent from client to server
 };
