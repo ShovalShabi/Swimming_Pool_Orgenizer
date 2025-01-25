76eb480a0001e57952ad4a35be59acd44236b745
ShovalShabi <shovalshabi@gmail.com>
2025-01-25 16:53:38 +0200

    \Checking faild MongoDB failed URL

--- backend/src/etc/db-connection.etc.ts
+++ backend/src/etc/db-connection.etc.ts
@@ -32,6 +32,7 @@ export const connectToDatabase = async (): Promise<void> => {
   } catch (error) {
     // Log connection error and exit process with error code
     logger.error("MongoDB connection error:", error);
+    console.log(`Failed mongo url: ${MONGO_URL}`);
     process.exit(1);
   }
 };
