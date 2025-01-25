6d4a6b4e3a2cf4b757fcd67a8e8dd22340c7574b
ShovalShabi <shovalshabi@gmail.com>
2025-01-25 18:44:10 +0200

    Removed unused print

--- backend/src/etc/db-connection.etc.ts
+++ backend/src/etc/db-connection.etc.ts
@@ -32,7 +32,6 @@ export const connectToDatabase = async (): Promise<void> => {
   } catch (error) {
     // Log connection error and exit process with error code
     logger.error("MongoDB connection error:", error);
-    console.log(`MongoDB URL fault: ${MONGO_URL}`);
     process.exit(1);
   }
 };
